dd076789f612494127de5e7b4e80ca2bed5ac573
Alxandr <alxandr@alxandr.me>
2014-08-16 11:28:38 +0000

    responsive that shit

---
+++ assets-src/js/launchscroll.js
@@ -0,0 +1,12 @@
+/**
+* Created with Blog.
+* User: Alxandr
+* Date: 2014-08-16
+* Time: 11:15 AM
+* To change this template use Tools | Templates.
+*/
+
+if(document.body.className.indexOf('home-template') === -1) {
+  var content = document.getElementById('content');
+  content.scrollIntoView(true);
+}
